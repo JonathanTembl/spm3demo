d975b4e036ad0d564be0cc3c114b39e5fc8b464e
Tembl Jonathan <Jonathan.Tembl@BSLINZ2-IT.LOCAL>
2023-11-30 14:15:04 +0100

    add m1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World, Git ist cool, oder? JA, heute neuer Commit");
+Console.WriteLine("Hello, World, Git ist cool, oder? JA, heute neuer Commit m1");
