ed95a880c6fdb20bf341f399576a453cc6a49b96
Tembl Jonathan <Jonathan.Tembl@BSLINZ2-IT.LOCAL>
2023-11-30 16:29:00 +0100

    rebase add f3 and f4

--- Program.cs
+++ Program.cs
@@ -1,3 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World, Git ist cool, oder? JA, heute neuer Commit m1 f1 remote change f2");
+Console.WriteLine("Hello, World, Git ist cool, oder? JA, heute neuer Commit m1 f1 remote change f2 rebased");
